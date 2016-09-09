42ea6ac9aef3f2c5e4d68e1ad15e38b91e2f2000
Brandon Hubbard <bkhubbard@gmail.com>
2016-09-09 12:49:34 -0700

    basic files

---
+++ wp-github-api.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* WP Github API
+*
+* @package WP-Github-API
+*/
+
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+/**
+ * Github API Class.
+ */
+class GithubAPI {
+
+}
