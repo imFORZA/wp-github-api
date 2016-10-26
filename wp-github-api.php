bd60d6dad182f842a16f106c12cc8bbbff44c50b
Brandon Hubbard <bhubbard@users.noreply.github.com>
2016-10-26 13:31:39 -0700

    Update wp-github-api.php

--- wp-github-api.php
+++ wp-github-api.php
@@ -5,12 +5,26 @@
 * @package WP-Github-API
 */
 
+/*
+* Plugin Name: WP Github API
+* Plugin URI: https://github.com/wp-api-libraries/wp-github-api
+* Description: Perform API requests to Github in WordPress.
+* Author: WP API Libraries
+* Version: 1.0.0
+* Author URI: https://wp-api-libraries.com
+* GitHub Plugin URI: https://github.com/wp-api-libraries/wp-github-api
+* GitHub Branch: master
+*/
+
 /* Exit if accessed directly */
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-/**
- * Github API Class.
- */
-class GithubAPI {
-
-}
+if ( ! class_exists( 'GithubAPI' ) {
+	
+	/**
+ 	* Github API Class.
+ 	*/
+	class GithubAPI {
+	} // End Class.
+	
+} // End Class Exists Check.
